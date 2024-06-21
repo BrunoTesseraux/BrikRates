d566115df739a98672802e67c98283d68fe67ac2
Nikolas Sretkovic <145345708+NikolaSretko@users.noreply.github.com>
2024-06-21 20:42:21 +0200

    freitagAbend_scraperSitzt

--- scraper/src/service/index.ts
+++
@@ -1,8 +0,0 @@
-import { getIncludio } from "./regensburg/includio/getIncludio";
-import { postIncludio } from "./regensburg/includio/postIncludio";
-
-
-export const ScraperService = {
-  getIncludio,
-  postIncludio
-}
